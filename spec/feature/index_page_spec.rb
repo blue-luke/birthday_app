6aa787d58783b1e15fe78f0e1c3eb9f25750ebb8
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-02-07 23:49:15 +0000

    Created dropdown for months, some tinkering elsewhere. Feature test for data entry complete, now need to link it up

--- spec/feature/index_page_spec.rb
+++ spec/feature/index_page_spec.rb
@@ -11,4 +11,13 @@ feature 'index page' do
     expect(page).to have_content 'Enter your name'
     fill_in('Name', with: 'Jane')
   end
+  scenario 'allows birth day entry' do
+    visit '/'
+    expect(page).to have_content 'Enter your birth day'
+    fill_in('Day', with: '1')
+  end
+  scenario 'allows birth month entry' do
+    visit '/'
+    select "January", :from => "month-names"
+  end
 end
