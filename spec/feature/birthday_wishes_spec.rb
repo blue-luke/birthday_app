5b71822d022512fb25610d38a98a96aca0709481
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-02-07 23:25:12 +0000

    Red on feature test, wishing a specific user happy birthday

--- spec/feature/birthday_wishes_spec.rb
+++ spec/feature/birthday_wishes_spec.rb
@@ -6,4 +6,9 @@ feature 'birthday wishes' do
     visit '/birthday_wishes'
     expect(page).to have_content 'Happy Birthday!'
   end
+  scenario 'wishes specific user happy birthday' do
+    visit '/'
+    fill_in('Name', with: 'Jane')
+    expect(page).to have_content 'Happy Birthday Alice!'
+  end
 end
