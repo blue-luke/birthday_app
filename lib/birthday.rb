d076a44f417563ab4f1ff33d59b540e0d759c22a
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-02-08 00:11:03 +0000

    Returning current day

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,4 +1,12 @@
 class Birthday
+  def initialize(name, day, month)
+    @name = name
+    @day = day
+    @month = month
+  end
+  def current_day
+    Time.now.strftime("%d")[1].to_i
+  end
   def message
     "Happy Birthday, Alice!"
   end
--- spec/birthday_spec.rb
+++ spec/birthday_spec.rb
@@ -2,7 +2,15 @@ require 'birthday'
 
 describe 'Birthday' do
   it 'tells you if your birthday is today' do
-    b = Birthday.new
+    b = Birthday.new("Alice", 8, "February")
     expect(b.message).to eq "Happy Birthday, Alice!"
+  end
+  # it 'tells you if your birthday is tomorrow' do
+  #   b = Birthday.new("Jane", 9, "February")
+  #   expect(b.message).to eq "Your birthday will be in 1 day, Jane."
+  # end
+  it 'tells you current day number' do
+    b = Birthday.new("Jane", 9, "February")
+    expect(b.current_day).to eq 8
   end
 end
