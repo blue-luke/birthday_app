c2049db4d3467bb051088ba5e7b8949a6428a409
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-02-08 14:18:37 +0000

    Lost track of what I'm doing and how, too much in my head and not supported by tests

--- lib/date_converter.rb
+++ lib/date_converter.rb
@@ -1,9 +1,20 @@
+require 'date'
+
 class DateConverter
+  def initialize
+    @parsed_text = nil
+  end
   def to_text(day, month, year)
     if day.to_s.length == 1
       day = "0#{day.to_s}"
     end
 
-    "#{day} #{month} #{year}"
+    @parsed_text = "#{day} #{month} #{year}"
+  end
+  def parse_text
+    Date.parse(@parsed_text)
+  end
+  def find_days_between(object1, object2)
+    (object1 - object2).to_i
   end
 end
